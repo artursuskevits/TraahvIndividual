10d04013df04ada9da9e500453abebe62b22bc1a
opilane <opilane@HP-CZC2349HTP>
2024-09-03 12:29:05 +0300

    Add project files.

---
+++ TraahvIndividual/Controllers/HomeController.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using TraahvIndividual.Models;
+
+namespace TraahvIndividual.Controllers
+{
+    public class HomeController : Controller
+    {
+        TrahvidContext db = new TrahvidContext();
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+
+        public ActionResult Traahv()
+        {
+            IEnumerable<Traahv> traahvs = db.Traahv.ToList();
+            return View(traahvs);
+        }
+        [HttpGet]
+        public ActionResult CreateTraahv()
+        {
+            return View();
+        }
+
+
+        public ActionResult CreateTraahv(Traahv trahv)
+        {
+            db.Traahv.Add(trahv);
+            db.SaveChanges();
+            return RedirectToAction("Traahv");
+        }
+
+        [HttpGet]
+        public ActionResult EditTraahv(int? id)
+        {
+            Traahv g = db.Traahv.Find(id);
+            if (g == null)
+            {
+                return HttpNotFound();
+
+            }
+            return View(g);
+        }
+        [HttpPost, ActionName("EditTraahv")]
+        public ActionResult EditTraavhConfirmed(Traahv guest)
+        {
+            db.Entry(guest).State = EntityState.Modified;
+            db.SaveChanges();
+            return RedirectToAction("Traahv");
+        }
+
+        public ActionResult DeleteTraahv(int id)
+        {
+            Traahv d = db.Traahv.Find(id);
+            if (d == null)
+            {
+                return HttpNotFound();
+
+            }
+            return View(d);
+        }
+        [HttpPost, ActionName("DeleteTraahv")]
+        public ActionResult DeleteTraahvConfirmed(int id)
+        {
+            Traahv d = db.Traahv.Find(id);
+            if (d == null)
+            {
+                return HttpNotFound();
+            }
+
+            db.Traahv.Remove(d);
+            db.SaveChanges();
+            return RedirectToAction("Traahv");
+        }
+    }
+}
---
+++ TraahvIndividual/Models/Traahv.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Data.Entity;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+using System.Web.UI.WebControls;
+
+namespace TraahvIndividual.Models
+{
+    [Table("Traahv")]
+    public class Traahv
+    {
+        [Key]
+        public int Id { get; set; }
+        public string SoidukeNumber { get; set; }
+        public string OmanikuNimi { get; set; }
+        public string OmanikuEpost { get; set; }
+        public DateTime Rikkumisekuupaev { get; set; }
+        public int KiiruseUletamine { get; set; }
+
+        public int TrahviSuurus { get; set; }
+        public virtual ICollection<Login> Logins { get; set; }
+    }
+}
